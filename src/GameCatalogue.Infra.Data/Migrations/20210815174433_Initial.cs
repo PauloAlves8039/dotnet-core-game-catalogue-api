d25f75159c958a40d438ebf81f0e2c639e29f625
PauloAlves8039 <pj38alves@gmail.com>
2021-08-15 14:51:08 -0300

    Aplicando o recurso de Migrations

---
+++ src/GameCatalogue.Infra.Data/Migrations/20210815174433_Initial.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameCatalogue.Infra.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Platforms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Platforms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Producer = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PlatformId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Games_Platforms_PlatformId",
+                        column: x => x.PlatformId,
+                        principalTable: "Platforms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Platforms",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Microsoft Windows" });
+
+            migrationBuilder.InsertData(
+                table: "Platforms",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Microsoft Xbox One" });
+
+            migrationBuilder.InsertData(
+                table: "Platforms",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3, "PlayStation 4" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_PlatformId",
+                table: "Games",
+                column: "PlatformId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "Platforms");
+        }
+    }
+}
---
+++ src/GameCatalogue.Infra.Data/Migrations/20210815174456_SeedGames.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameCatalogue.Infra.Data.Migrations
+{
+    public partial class SeedGames : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Games(Name,Producer,Gender,Quantity,Image,PlatformId) " +
+            "VALUES('Resident Evil 7','Capcom Ltd','Action, Fear',1,'resident-evil-7.png',1)");
+
+            mb.Sql("INSERT INTO Games(Name,Producer,Gender,Quantity,Image,PlatformId) " +
+            "VALUES('Hellblade: Senua s Sacrifice','Ninja Theory','Action, Epic',1,'hellblade.png',2)");
+
+            mb.Sql("INSERT INTO Games(Name,Producer,Gender,Quantity,Image,PlatformId) " +
+            "VALUES('God Of War','Santa Monica Studio','Action, Epic',1,'god-of-war.png',3)");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Games");
+        }
+    }
+}
