2077bfb8537db4d0946ae653b12fd28595d126d4
PauloAlves8039 <pj38alves@gmail.com>
2021-08-15 12:24:48 -0300

    Registrando os serviços do contexto e dos repositórios

--- src/GameCatalogue.API/Startup.cs
+++ src/GameCatalogue.API/Startup.cs
@@ -1,3 +1,4 @@
+using GameCatalogue.Infra.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,7 +26,7 @@ namespace GameCatalogue.API
 
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddInfrastructure(Configuration);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
---
+++ src/GameCatalogue.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using GameCatalogue.Infra.Data.Context;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using GameCatalogue.Domain.Interfaces;
+using GameCatalogue.Infra.Data.Repositories;
+
+namespace GameCatalogue.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration) 
+        {
+            services.AddDbContext<ApplicationDbContext>(options => 
+             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"
+            ), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<IPlatformRepository, PlatformRepository>();
+            services.AddScoped<IGameRepository, GameRepository>();
+
+            return services;
+        }
+    }
+}
