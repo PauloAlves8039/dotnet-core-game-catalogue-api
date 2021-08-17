c4fb03efa00fd6dfa939b69de0394df989d072e1
PauloAlves8039 <pj38alves@gmail.com>
2021-08-17 13:52:19 -0300

    Criando classes Queries para a entidade Game

---
+++ src/GameCatalogue.Application/Games/Queries/GetGameByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using GameCatalogue.Domain.Entities;
+using MediatR;
+
+namespace GameCatalogue.Application.Games.Queries
+{
+    public class GetGameByIdQuery : IRequest<Game>
+    {
+        public int Id { get; set; }
+
+        public GetGameByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ src/GameCatalogue.Application/Games/Queries/GetGamesQuery.cs
@@ -0,0 +1,10 @@
+﻿using GameCatalogue.Domain.Entities;
+using MediatR;
+using System.Collections.Generic;
+
+namespace GameCatalogue.Application.Games.Queries
+{
+    public class GetGamesQuery : IRequest<IEnumerable<Game>>
+    {
+    }
+}
