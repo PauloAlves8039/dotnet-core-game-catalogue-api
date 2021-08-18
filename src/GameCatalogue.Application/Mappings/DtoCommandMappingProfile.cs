6e32475179513d030bb33c55ca1d67b7692416d0
PauloAlves8039 <pj38alves@gmail.com>
2021-08-17 21:56:13 -0300

    Definindo a classe DtoCommandMappingProfile

---
+++ src/GameCatalogue.Application/Mappings/DtoCommandMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using GameCatalogue.Application.DTOs;
+using GameCatalogue.Application.Games.Commands;
+
+namespace GameCatalogue.Application.Mappings
+{
+    public class DtoCommandMappingProfile : Profile
+    {
+        public DtoCommandMappingProfile()
+        {
+            CreateMap<GameDto, GameCreateCommand>();
+            CreateMap<GameDto, GameUpdateCommand>();
+        }
+    }
+}
