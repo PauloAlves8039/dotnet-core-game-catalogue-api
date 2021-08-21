a5cb792304ec92580f23304676c463cd64fd04f7
PauloAlves8039 <pj38alves@gmail.com>
2021-08-21 13:46:39 -0300

    Ativando a autenticação e validando o Token JWT

--- src/GameCatalogue.API/Startup.cs
+++ src/GameCatalogue.API/Startup.cs
@@ -1,17 +1,10 @@
 using GameCatalogue.Infra.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GameCatalogue.API
 {
@@ -27,6 +20,7 @@ namespace GameCatalogue.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddInfrastructure(Configuration);
+            services.AddInfrastructureJWT(Configuration);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
---
+++ src/GameCatalogue.Infra.IoC/DependencyInjectionJWT.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
+
+namespace GameCatalogue.Infra.IoC
+{
+    public static class DependencyInjectionJWT
+    {
+        public static IServiceCollection AddInfrastructureJWT(this IServiceCollection services, IConfiguration configuration) 
+        {
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidAudience = configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:SecretKey"])),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+            return services;
+        }
+    }
+}
